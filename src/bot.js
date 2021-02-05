18d577e27c955dbdf3c4ba0771c9ac58b3f44397
unknown <danedjackson@yahoo.com>
2021-02-04 23:13:40 -0500

    up

--- src/bot.js
+++ src/bot.js
@@ -15,13 +15,14 @@ const ftpusername = process.env.FTPUSERNAME;
 const ftppassword = process.env.FTPPASSWORD;
 
 var steamID;
+var dinoName;
 
 console.log(prefix);
 //Create an instance of client
 const client = new Client();
 const ftpClient = new ftp.Client();
 
-async function ftpConnection(steamID) {
+async function ftpDownload() {
     ftpClient.ftp.verbose = true;
     ftpClient.ftp.ipFamily = 4;
     try {
@@ -33,7 +34,7 @@ async function ftpConnection(steamID) {
         });
         console.log(await ftpClient.ftp.pwd);
         console.log(await ftpClient.list());
-        await ftpClient.downloadTo("Test.zip", "512KB.zip");
+        await ftpClient.downloadTo(steamID + ".zip", "512KB.zip");
         // await ftpClient.downloadTo("Injection.json", steamID + ".json");
     } catch(err){
         console.error(err);
@@ -48,11 +49,41 @@ function editJson() {
     try {
         contents = JSON.parse(data);
         contents.Growth = "1.0";
+        contents.Hunger = "9999";
+        contents.Thirst = "9999";
+        contents.Stamina = "9999";
     } catch (err) {
         console.error(err);
     }
     console.log(contents);
-    fs.writeFileSync("output.json", JSON.stringify(contents));
+    fs.writeFileSync(steamID + ".json", JSON.stringify(contents));
+    ftpUpload();
+}
+
+async function ftpUpload() {
+    console.log("Uploading file. . .")
+    ftpClient.ftp.verbose = true;
+    ftpClient.ftp.ipFamily = 4;
+    try {
+        await ftpClient.access({
+            host: ftpLocation,
+            port: ftpPort,
+            user: ftpusername,
+            password: ftppassword
+        });
+        await ftpClient.uploadFrom(steamID + ".json", "./upload/" + steamID + ".json");
+    } catch(err){
+        console.error(err);
+    }
+    ftpClient.close();
+    deleteLocalFile();
+}
+
+async function deleteLocalFile() {
+    console.log("Deleting local files . . .");
+    fs.unlink("./" + steamID + ".json", (err) => {
+        if (err) throw err;
+    });
 }
 
 client.on("ready", () => {
@@ -71,8 +102,9 @@ client.on("message", async message => {
 
     if (command === prefix+"inject") {
         steamID = args[0];
+        dinoName = args[1];
         console.log(`In if block\n${message.author.tag}: ${message.content}`);
-        ftpConnection(steamID);
+        ftpDownload();
     }
 });
 
